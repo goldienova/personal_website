005912cd855401e6c43e36cbb322c963033f5712
Mariana Templin <mariana.acuna.templin@gmail.com>
2017-05-23 23:26:58 -0400

    fixed links

--- browser/react/navBar.jsx
+++ browser/react/navBar.jsx
@@ -5,7 +5,7 @@ export default function NavBar(){
   return(
          <div>
     <ul className="navBar">
-       <li><Link to="/code">Code</Link></li>
+       <li><Link to="/">Code</Link></li>
        <li><a href="../art/index.html">Art</a></li>
         <li><Link to="/about">About</Link></li>
     </ul>
